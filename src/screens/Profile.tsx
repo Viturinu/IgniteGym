55fa2694bb421698c663e6ab4f83e4a777224867
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-02-27 22:50:27 -0300

    ultimo modulo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,14 +2,50 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker"
+import * as FileSystem from "expo-file-system"
 
 export function Profile() {
 
     const PHOTO_SIZE = 33;
-    const [photoIsLoading, setPhotoIsLoading] = useState(true);
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
+    const toast = useToast();
+
+    const [userPhoto, setUserPhoto] = useState("https://github.com/viturinu.png")
+
+    async function handleUserPhotoSelect() {
+        setPhotoIsLoading(true);
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true,
+            });
+            if (photoSelected.canceled) {
+                return;
+            }
+            if (photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri, { md5: true, size: true });
+
+                if (photoInfo.exists && (photoInfo.size / 1024 / 1024) > 0.001) { //sempre verificar uma promisse, pois pode dar algum erro ao tentar acessar certas propriedades sem a devida verificação
+                    return toast.show({
+                        title: "Essa imagem é muito grande, escolha uma de até 5Mb",
+                        placement: "top",
+                        bgColor: "red.500"
+                    })
+                }
+                setUserPhoto(photoSelected.assets[0].uri);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setPhotoIsLoading(false);
+        }
+    }
 
     return (
         <VStack flex={1}>
@@ -22,14 +58,15 @@ export function Profile() {
                             h={PHOTO_SIZE}
                             rounded="full"
                             startColor="gray.500"
-                            endColor="gray.400" />
+                            endColor="gray.400"
+                        />
                         : <UserPhoto
-                            source={{ uri: "https://github.com/viturinu.png" }}
+                            source={{ uri: userPhoto }}
                             size={PHOTO_SIZE}
                             alt="Foto do usuário"
                         />
                     }
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
                             Alterar foto
                         </Text>
