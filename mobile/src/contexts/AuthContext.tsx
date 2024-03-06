179232bed8494605c4c630283cc408b913993eb4
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-03-06 19:07:28 -0300

    jwt done

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { UserDTO } from "@dtos/userDTO";
 import { api } from "@services/api";
-import { storageAuthTokenSabe } from "@storage/storageAuthToken";
+import { storageAuthTokenGet, storageAuthTokenRemove, storageAuthTokenSave } from "@storage/storageAuthToken";
 import { storageUserGet, storageUserRemove, storageUserSave } from "@storage/storageUser";
 import { ReactNode, createContext, useEffect, useState } from "react";
 
@@ -23,14 +23,19 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) { //
     const [token, setToken] = useState("");
     const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
-    async function storageUserAndToken(userData: UserDTO, token: string) {
+    async function signIn(email: string, password: string) { //ao inves de mandar setUser pra outra tela, a gente centraliza a lógica toda aqui nesse contexto, mandando o signIn (setUser() fica dentro do signIn()) apenas.
         try {
-            setIsLoadingUserStorageData(true);
+            const { data } = await api.post("/sessions", { //chama API pra buscar as informações
+                email,
+                password
+            });
 
-            setToken(token);
-            setUser(userData);
-            await storageUserSave(userData);
-            await storageAuthTokenSabe(token);
+            if (data.user && data.token) { //check se voltou os resultados de usuario e token de usuário
+
+                setIsLoadingUserStorageData(true);
+                await storageUserAndTokenSave(data.user, data.token); //salva os dados no AsyncStorage
+                await userAndTokenUpdate(data.user, data.token); //salva as variáveis nos estados
+            }
         } catch (error) {
             throw error;
         } finally {
@@ -39,26 +44,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) { //
 
     }
 
-    async function signIn(email: string, password: string) { //ao inves de mandar setUser pra outra tela, a gente centraliza a lógica toda aqui nesse contexto, mandando o signIn (setUser() fica dentro do signIn()) apenas.
-        try {
-            const { data } = await api.post("/sessions", {
-                email,
-                password
-            });
-
-            if (data.user && data.token) {
-                storageUserAndToken(data.user, data.token);
-            }
-        } catch (error) {
-            throw error;
-        }
-
-    }
-
     async function signOut() {
         try {
             setIsLoadingUserStorageData(true)
+            //Atualiza os estados pra vazios
             setUser({} as UserDTO);
+            setToken("");
+            //Remove do AsyncStorage os dados de usuário e token de usuário
+            await storageAuthTokenRemove();
             await storageUserRemove();
         } catch (error) {
             throw error;
@@ -68,11 +61,35 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) { //
 
     }
 
+    async function userAndTokenUpdate(userData: UserDTO, token: string) { //vai atualizar os estados e cabeçalho de requisições da aplicação tanto na reinicialização quanto no login ()
+        //took out try catch cause it is just an update info according to teacher
+        api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+        setToken(token);
+        setUser(userData);
+    }
+
+    async function storageUserAndTokenSave(userData: UserDTO, token: string) { //vai salvar no AsyncStorage pra persistir os dados no mobile
+        try {
+            setIsLoadingUserStorageData(true);
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token); //frufru na minha opiniao; podia ser feito aqui mesmo;
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
     async function loadUserData() { //será carregada sempre que reiniciarmos o APP, realimentará todos os nossos estados e renderizações serão todas feitas novamente
         try {
-            const userLogged = await storageUserGet();
-            if (userLogged) {
-                setUser(userLogged);
+            setIsLoadingUserStorageData(true);
+
+            const userLogged = await storageUserGet(); //busca informações de usuário, na reinicialização do app ou reabertura, pois useEffect chama loadUserData()
+            const token = await storageAuthTokenGet(); //busca token de usuário, na reinicialização do app ou reabertura, pois useEffect chama loadUserData()
+            if (token && userLogged) { //confere se chegou algo aqui nessas variáveis
+                userAndTokenUpdate(user, token); //se chegou ele vai atualizar os estados (não precisa escrever, pois ele buscou de lá mesmo, do AsyncStorage)
+
             }
         } catch (error) {
             throw error;
